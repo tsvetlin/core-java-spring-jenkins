01a4f70e88d8b966bf32a86873a83c5c3114a3d5
Szvetlin Tanyi <svetlintanyi@gmail.com>
2021-04-27 23:29:39 +0200

    buils modified

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -11,31 +11,31 @@ cp -f $WORKSPACE/serviceregistry/target/*.jar $WORKSPACE/jenkins/build/servicere
 # Copy the Auth jar to the build location
 rm -rf $WORKSPACE/jenkins/build/authorization
 mkdir $WORKSPACE/jenkins/build/authorization
-cp -f $WORKSPACE//authorization/target/*.jar $WORKSPACE/jenkins/build/authorization
+cp -f $WORKSPACE/authorization/target/*.jar $WORKSPACE/jenkins/build/authorization
 # cp -f authorization/target/*.properties jenkins/build/authorization
 
 # Copy the ORCH jar to the build location
 rm -rf $WORKSPACE/jenkins/build/orchestrator
 mkdir $WORKSPACE/jenkins/build/orchestrator
-cp -f $WORKSPACE//orchestrator/target/*.jar $WORKSPACE/jenkins/build/orchestrator
+cp -f $WORKSPACE/orchestrator/target/*.jar $WORKSPACE/jenkins/build/orchestrator
 # cp -f orchestrator/target/*.properties jenkins/build/orchestrator
 
 # Copy the Gatekeeper jar to the build location
 rm -rf $WORKSPACE/jenkins/build/gatekeeper
 mkdir $WORKSPACE/jenkins/build/gatekeeper
-cp -f $WORKSPACE//gatekeeper/target/*.jar $WORKSPACE/jenkins/build/gatekeeper
+cp -f $WORKSPACE/gatekeeper/target/*.jar $WORKSPACE/jenkins/build/gatekeeper
 # cp -f gatekeeper/target/*.properties jenkins/build/gatekeeper
 
 # Copy the Gateway jar to the build location
 rm -rf $WORKSPACE/jenkins/build/gateway
 mkdir $WORKSPACE/jenkins/build/gateway
-cp -f $WORKSPACE//gateway/target/*.jar $WORKSPACE/jenkins/build/gateway
+cp -f $WORKSPACE/gateway/target/*.jar $WORKSPACE/jenkins/build/gateway
 # cp -f gateway/target/*.properties jenkins/build/gateway
 
 # Copy the Event Handler jar to the build location
 rm -rf $WORKSPACE/jenkins/build/eventhandler
 mkdir $WORKSPACE/jenkins/build/eventhandler
-cp -f $WORKSPACE//eventhandler/target/*.jar $WORKSPACE/jenkins/build/eventhandler
+cp -f $WORKSPACE/eventhandler/target/*.jar $WORKSPACE/jenkins/build/eventhandler
 # cp -f eventhandler/target/*.properties jenkins/build/eventhandler
 
 echo "****************************"
