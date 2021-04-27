652fe0aaa9c1cecb0a7b8d7a65b9e96ca113291b
Szvetlin Tanyi <svetlintanyi@gmail.com>
2021-04-27 23:16:17 +0200

    build update

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -1,43 +1,45 @@
 #!/bin/bash
 
+WORKSPACE=/var/jenkins_home/workspace/Arrowhead/Arrowhead_Core_Spring
+
 # Copy the SR jar to the build location
-rm -rf jenkins/build/serviceregistry
-mkdir jenkins/build/serviceregistry
-cp -f serviceregistry/target/*.jar jenkins/build/serviceregistry
+rm -rf $WORKSPACE/jenkins/build/serviceregistry
+mkdir $WORKSPACE/jenkins/build/serviceregistry
+cp -f $WORKSPACE/serviceregistry/target/*.jar $WORKSPACE/jenkins/build/serviceregistry
 # cp -f serviceregistry/target/*.properties jenkins/build/serviceregistry
 
 # Copy the Auth jar to the build location
-rm -rf jenkins/build/authorization
-mkdir jenkins/build/authorization
-cp -f /authorization/target/*.jar jenkins/build/authorization
+rm -rf $WORKSPACE/jenkins/build/authorization
+mkdir $WORKSPACE/jenkins/build/authorization
+cp -f $WORKSPACE//authorization/target/*.jar $WORKSPACE/jenkins/build/authorization
 # cp -f authorization/target/*.properties jenkins/build/authorization
 
 # Copy the ORCH jar to the build location
-rm -rf jenkins/build/orchestrator
-mkdir jenkins/build/orchestrator
-cp -f /orchestrator/target/*.jar jenkins/build/orchestrator
+rm -rf $WORKSPACE/jenkins/build/orchestrator
+mkdir $WORKSPACE/jenkins/build/orchestrator
+cp -f $WORKSPACE//orchestrator/target/*.jar $WORKSPACE/jenkins/build/orchestrator
 # cp -f orchestrator/target/*.properties jenkins/build/orchestrator
 
 # Copy the Gatekeeper jar to the build location
-rm -rf jenkins/build/gatekeeper
-mkdir jenkins/build/gatekeeper
-cp -f /gatekeeper/target/*.jar jenkins/build/gatekeeper
+rm -rf $WORKSPACE/jenkins/build/gatekeeper
+mkdir $WORKSPACE/jenkins/build/gatekeeper
+cp -f $WORKSPACE//gatekeeper/target/*.jar $WORKSPACE/jenkins/build/gatekeeper
 # cp -f gatekeeper/target/*.properties jenkins/build/gatekeeper
 
 # Copy the Gateway jar to the build location
-rm -rf jenkins/build/gateway
-mkdir jenkins/build/gateway
-cp -f /gateway/target/*.jar jenkins/build/gateway
+rm -rf $WORKSPACE/jenkins/build/gateway
+mkdir $WORKSPACE/jenkins/build/gateway
+cp -f $WORKSPACE//gateway/target/*.jar $WORKSPACE/jenkins/build/gateway
 # cp -f gateway/target/*.properties jenkins/build/gateway
 
 # Copy the Event Handler jar to the build location
-rm -rf jenkins/build/eventhandler
-mkdir jenkins/build/eventhandler
-cp -f /eventhandler/target/*.jar jenkins/build/eventhandler
+rm -rf $WORKSPACE/jenkins/build/eventhandler
+mkdir $WORKSPACE/jenkins/build/eventhandler
+cp -f $WORKSPACE//eventhandler/target/*.jar $WORKSPACE/jenkins/build/eventhandler
 # cp -f eventhandler/target/*.properties jenkins/build/eventhandler
 
 echo "****************************"
 echo "** Building Docker Images **"
 echo "****************************"
 
-cd jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache
+cd $WORKSPACE/jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache
