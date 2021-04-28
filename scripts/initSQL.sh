2cc0b4ae064f52db5400e86e6cbcf17239590cd3
Szvetlin Tanyi <svetlintanyi@gmail.com>
2021-04-28 20:26:15 +0200

    4.3.0

--- scripts/initSQL.sh
+++ scripts/initSQL.sh
@@ -4,20 +4,22 @@ rm -rf sql
 
 mkdir sql
 cd sql
-wget https://raw.githubusercontent.com/tsvetlin/core-java-spring-jenkins/master/scripts/create_empty_arrowhead_db.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/create_empty_arrowhead_db.sql
 sed -i 's/source /& docker-entrypoint-initdb.d\/privileges\//g' create_empty_arrowhead_db.sql
 
 mkdir privileges
 cd privileges
-wget https://raw.githubusercontent.com/tsvetlin/core-java-spring-jenkins/master/scripts/create_arrowhead_tables.sql
-wget https://raw.githubusercontent.com/tsvetlin/core-java-spring-jenkins/master/scripts/service_registry_privileges.sql
-wget https://raw.githubusercontent.com/tsvetlin/core-java-spring-jenkins/master/scripts/authorization_privileges.sql
-wget https://raw.githubusercontent.com/tsvetlin/core-java-spring-jenkins/master/scripts/orchestrator_privileges.sql
-wget https://raw.githubusercontent.com/tsvetlin/core-java-spring-jenkins/master/scripts/event_handler_privileges.sql
-wget https://raw.githubusercontent.com/tsvetlin/core-java-spring-jenkins/master/scripts/choreographer_privileges.sql
-wget https://raw.githubusercontent.com/tsvetlin/core-java-spring-jenkins/master/scripts/gatekeeper_privileges.sql
-wget https://raw.githubusercontent.com/tsvetlin/core-java-spring-jenkins/master/scripts/gateway_privileges.sql
-wget https://raw.githubusercontent.com/tsvetlin/core-java-spring-jenkins/master/scripts/certificate_authority_privileges.sql
-wget https://raw.githubusercontent.com/tsvetlin/core-java-spring-jenkins/master/scripts/system_registry_privileges.sql
-wget https://raw.githubusercontent.com/tsvetlin/core-java-spring-jenkins/master/scripts/device_registry_privileges.sql
-wget https://raw.githubusercontent.com/tsvetlin/core-java-spring-jenkins/master/scripts/onboarding_controller_privileges.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/authorization_privileges.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/certificate_authority_privileges.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/choreographer_privileges.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/create_arrowhead_tables.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/device_registry_privileges.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/event_handler_privileges.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/gatekeeper_privileges.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/gateway_privileges.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/onboarding_controller_privileges.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/orchestrator_privileges.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/qos_monitor_privileges.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/service_registry_privileges.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/system_registry_privileges.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/translator_privileges.sql
